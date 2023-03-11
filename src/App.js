ca19fea1b312ff844f461924695114ed863154d1
Moccasym <57842536+Moccasym@users.noreply.github.com>
2023-03-11 15:42:27 +0100

    added Rank component and tried particles/ not working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+//import Particle from './components/Particles/Particles';
+import Particles from "react-tsparticles";
+
+
+
 
 function App() {
+  
   return (
     <div className="App">
+      <Particles id="tsparticles" url="http://foo.bar/particles.json"  />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/*<FaceRecognition /> */}
     </div>
