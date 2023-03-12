7cd13e23201ae2a58c661aeae2b6ec72c267c1e5
Moccasym <57842536+Moccasym@users.noreply.github.com>
2023-03-12 19:16:47 +0100

    adding clarifai api for face recognition /inprocess

--- src/App.js
+++ src/App.js
@@ -1,27 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import Particle from './components/Particles/Particles';
+import Clarifai from 'clarifai';
 
+process.nextTick = setImmediate;
 
+const app = new Clarifai.App({
+  apiKey: 'b4f74c7cfa714ef1bc048820c65660ee'
+ });
 
-
-
-function App() {
+ ClarifaiApp.models.predict(Clarifai.GENERAL_MODEL, "https://samples.clarifai.com/metro-north.jpg").then(
+      function(response) {
+        // do something with response
+        console.log(response);
+      },
+      function(err) {
+        console.log(err);
+        // there was an error
+      }
+    );
   
-  return (
-    <div className="App">
-      <Particle className='particle'/>
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*<FaceRecognition /> */}
-    </div>
-  );
-}
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input:'',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particle className='particle'/>
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        {/*<FaceRecognition /> */}
+      </div>
+    );
+  }
+}
 export default App;
