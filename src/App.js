dfdf5341cdcd3b12ff12248c45662283c3328f07
Moccasym <57842536+Moccasym@users.noreply.github.com>
2023-03-13 11:37:06 +0100

    added box parameters to console.log /not working

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 
 import Clarifai from 'clarifai';
 
-process.nextTick = setImmediate;
+//process.nextTick = setImmediate;
 
 const app = new Clarifai.App({
   apiKey: 'b4f74c7cfa714ef1bc048820c65660ee'
@@ -24,28 +24,45 @@ class App extends Component {
     super();
     this.state = {
       input:'',
-      imageUrl: ''
+      imageUrl: '',
+      box: {}
     }
   }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box})
+  }
+
 
   onInputChange = (event) => {
     this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input})
-    app.models.predict(
-      Clarifai.FACE_DETECT_MODEL, 
-      this.state.input)
-      .then(
-      function(response) {
-        console.log('hi');
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      function(err) {
-      }
-    );
+    this.setState({imageUrl: this.state.input});
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL, 
+        this.state.input)
+        .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
 
+          // console.log('hi');
+          // console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+        .catch(err =>console.log(err))
   }
 
   render() {
