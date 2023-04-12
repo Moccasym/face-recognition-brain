ebe45fb2ffd725048111df532a8a255a1dee3099
Moccasym <57842536+Moccasym@users.noreply.github.com>
2023-04-12 13:38:37 +0200

    cleaned up code, all working!

--- src/App.js
+++ src/App.js
@@ -119,34 +119,6 @@ class App extends Component {
         })
         .catch(err => console.log(err));
   }
-  //new Clarifai for later
-  //   app.models
-  //     .predict(
-  //       {
-  //         id: 'face-detection',
-  //         name: 'face-detection',
-  //         version: '6dc7e46bc9124c5c8824be4822abe105',
-  //         type: 'visual-detector',
-  //       }, this.state.input)
-  //     .then(response => {
-  //       console.log('hi', response)
-  //       if (response) {
-  //         fetch('http://localhost:3000/image', {
-  //           method: 'put',
-  //           headers: {'Content-Type': 'application/json'},
-  //           body: JSON.stringify({
-  //             id: this.state.user.id
-  //           })
-  //         })
-  //           .then(response => response.json())
-  //           .then(count => {
-  //             this.setState(Object.assign(this.state.user, { entries: count}))
-  //           })
-
-  //       }
-  //       this.displayFaceBox(this.calculateFaceLocation(response))
-  //     })
-  //     .catch(err => console.log(err));
 
 onRouteChange = (route) => {
   if (route === 'SignOut'){
