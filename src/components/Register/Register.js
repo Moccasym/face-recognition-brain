109a39207e89df7bba6134f29b9fb7682186ad91
Moccasym <57842536+Moccasym@users.noreply.github.com>
2023-04-15 00:14:34 +0200

    forget to change all fetches

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -24,7 +24,7 @@ class Register extends React.Component {
     };
 
     onSubmitSignUp = () => {
-        fetch('http://localhost:3000/register', {
+        fetch('https://face-detection-brain-api.onrender.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
         this.setState({signInPassword: event.target.value})
     };
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://face-detection-brain-api.onrender.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
